853ba58ca3a09aa98a66e780cda8db1b06122c8a
Anant Moudgalya <anant9798@gmail.com>
2020-07-29 11:39:48 +0530

    Implemented custom scopes for portfolio items

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,3 @@
 class Skill < ApplicationRecord
-    validates_presence_of :title, percent_util
+    validates_presence_of :title, :percent_utilized
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
 Blog.create!(
     title: "My Blog Post #{blog}",
@@ -15,19 +23,29 @@ end
 puts "10 blog posts created"
 
 5.times do |skill|
-Skill.create!(
-    title: "Rails #{skill}",
-    percent_util: 15
-)
-end
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+    end
 
 puts "5 skills created"
 
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
     Portfolio.create!(
         title: "Portfolio Title: #{portfolio_item}",
-        subtitle: "My Great Service",
+        subtitle: "Ruby on Rails",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image: "https://via.placeholder.com/600x400",
+        thumb_image: "https://via.placeholder.com/350x200"
+    )
+end
+
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio Title: #{portfolio_item}",
+        subtitle: "Angular",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         main_image: "https://via.placeholder.com/600x400",
         thumb_image: "https://via.placeholder.com/350x200"
